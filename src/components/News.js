b4f45b42e3d0411df0139685d603137bb559e030
ersouravbansal <souravbansal277@gmail.com>
2022-07-05 14:29:06 +0530

    Updated NewsItem and News component to display listing of news

--- src/components/News.js
+++ src/components/News.js
@@ -4,16 +4,41 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a news component.
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+      <div className='container my-3'>   
+      <h2>NewsKindle - Top Headlines</h2> 
+      <div className="row">
+        <div className="col-md-4">
+            <NewsItem title= "MyTitle01" description="mydesc"/>
+        </div>
+        <div className="col-md-4">
+            <NewsItem title= "MyTitle02" description="mydesc"/>
+        </div>
+        <div className="col-md-4">
+            <NewsItem title= "MyTitle03" description="mydesc"/>
+        </div>
+
+    </div> 
+    <div className="row">
+    <div className="col-md-4">
+            <NewsItem title= "MyTitle11" description="mydesc"/>
+        </div>
+        <div className="col-md-4">
+            <NewsItem title= "MyTitle12" description="mydesc"/>
+        </div>
+        <div className="col-md-4">
+            <NewsItem title= "MyTitle13" description="mydesc"/>
+        </div>
+
+    </div>
+    <div className="row">
+        <div className="col-md-4">
+            <NewsItem title= "MyTitle21" description="mydesc"/>
+        </div>
+        <div className="col-md-4">
+            <NewsItem title= "MyTitle22" description="mydesc"/>
+        </div>
+
+    </div>   
       </div>
     )
   }
