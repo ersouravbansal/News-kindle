b233d6439187d3a8b30b24d0557397615ab97d46
ersouravbansal <souravbansal277@gmail.com>
2022-07-05 12:03:00 +0530

    Removed React Default logo, Default CSS, Added Bootstrap and Navbar component.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import NavBar from './components/NavBar';
 
 export default class App extends Component {
   x='sourav';
   render() {
     return (
       <div>
-        Hello My First class Based Component. {this.x}
+        <NavBar/>
       </div>
     )
   }
