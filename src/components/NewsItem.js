0679576de148aca7e5b1a4d98ed07e1e9aa311cb
ersouravbansal <souravbansal277@gmail.com>
2022-07-05 18:39:20 +0530

    Using fetch API to populate news. And Removed static array of news. Applied componentDidMount Async Await.

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,10 +6,10 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
                 <div className="card" style={{width: "18rem"}}>
-                    <img src={imageUrl} className="card-img-top" alt="..."/>
+                    <img src={!imageUrl?"https://cdn.zeebiz.com/sites/default/files/2022/07/05/188452-untitled-design-2022-07-05t121042063.jpg":imageUrl} className="card-img-top" alt="..."/>
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
-                        <p className="card-text">{description}...</p>
+                        <p className="card-text">{description}</p>
                         <a href={newsUrl} target= "_blank"rel='noreferrer'  className="btn btn-sm btn-primary">Read More</a>
                     </div>
                 </div>
