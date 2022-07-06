0e04beb67b2ace0b3644ca1833b7c449d64d64fb
ersouravbansal <souravbansal277@gmail.com>
2022-07-06 16:30:03 +0530

    Installed React Router Dom, Fetching news Categorywise using React Router.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,31 @@ import './App.css';
 import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 export default class App extends Component {
   x='sourav';
   render() {
     return (
-      <div>
+      <>
+        <Router>
         <NavBar/>
-        <News country='in' pageSize={12} category='sports'/>
-      </div>
+        <Routes>
+          <Route exact path="/" element={<News key="general" country='in' pageSize={12} category='general'/>}/>
+          <Route exact path="/business" element={<News key="business" country='in' pageSize={12} category='business'/>}/>
+          <Route exact path="/entertainment" element={<News key="entertainment" country='in' pageSize={12} category='entertainment'/>}/>
+          <Route exact path="/general" element={<News key="general" country='in' pageSize={12} category='general'/>}/>
+          <Route exact path="/health" element={<News key="health" country='in' pageSize={12} category='health'/>}/>
+          <Route exact path="/science" element={<News key="science" country='in' pageSize={12} category='science'/>}/>
+          <Route exact path="/sports" element={<News key="sports" country='in' pageSize={12} category='sports'/>}/>
+          <Route exact path="/technology" element={<News key="technology" country='in' pageSize={12} category='technology'/>}/>
+        </Routes> 
+        </Router>
+      </>
     )
   }
 }
